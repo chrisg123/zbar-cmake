142a5e41edc4b20f96d9042c4c8884e6b59414ce
chrisg123 <chrisg_123@hotmail.com>
2019-09-08 10:38:50 -0400

    convert config.h.in to cmake with python script

--- tmp.py
+++ tmp.py
@@ -1,11 +1,29 @@
-f = open("zbar/config.h.in", "r")
-         
-print "Opened file"
+import re
 
-for l in f:    
-    print line
+def chomp(x):
+    if x.endswith("\r\n"): return x[:-2]
+    if x.endswith("\n") or x.endswith("\r"): return x[:-1]
+    return x
+
+inf = open("include/config.h.in", "r")
+print "Opened in file"
+
+outf = open("include/config.h.in.2", "w")
+print "Opened out file"
+
+for l in inf:
+    p = re.compile('(?<=#cmakedefine ).*$')
+    m = p.search(l)
+    if m != None:
+        s = m.group()
+        outf.write(chomp(l) + " @" + s + "@\n")
+    else:
+        outf.write(l)
+    print(l)
 
 
 
-f.close()
-print "Closed file"
+inf.close()
+print "Closed in file"
+outf.close()
+print "Closed out file"
